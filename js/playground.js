e32fdc81cecde27911a8db566464c8d23e77b329
tnmquan9799 <tnmquan9799@gmail.com>
2020-12-25 17:49:51 +0700

    Update chu' be' thich' nhay hip hop

--- js/playground.js
+++ js/playground.js
@@ -1,83 +1,203 @@
 var playground = function() {
-    var scene = new THREE.Scene();
-    var cam = new THREE.PerspectiveCamera(45, innerWidth/innerHeight, 1, 1000);
-    var renderer = new THREE.WebGLRenderer();
-    scene.background = new THREE.Color(0xfafafa);
-    renderer.setSize(innerWidth, innerHeight);
-    cam.position.x = -16;
-    cam.position.y = 11;
-    cam.position.z = -8;
-    document.body.appendChild(renderer.domElement);
-    let clock = new THREE.Clock();
+    const objects = [];
+    let scene, renderer;
+    let camera = new THREE.PerspectiveCamera(45, innerWidth/innerHeight, 1, 1000);
+    let controls = new THREE.PointerLockControls(camera, document.body);
+    let raycaster;
     let mixerBook;
-    var camPosition = new THREE.Vector3();
+    let clock = new THREE.Clock();
+
+    let moveForward = false;
+    let moveBackward = false;
+    let moveLeft = false;
+    let moveRight = false;
+    let canJump = false;
+
+    let prevTime = performance.now();
     const velocity = new THREE.Vector3();
+    const direction = new THREE.Vector3();
+    const color = new THREE.Color();
 
-    var directionalLight = new THREE.DirectionalLight({color: 0xFFFFFFF, intensity: 100});
-    directionalLight.position.set(0, 1, 0);
-    directionalLight.castShadow = true;
-    scene.add(directionalLight);
-    var ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
-    scene.add(ambientLight);
-    scene.updateMatrixWorld(true);
 
-    let controls = new THREE.PointerLockControls(cam, renderer.domElement);
+    init();
+    animate();
 
-    let keyboard = [];
-    addEventListener('keydown', (e) => {
-        keyboard[e.key] = true;
-    });
-    addEventListener('keyup', (e) => {
-        keyboard[e.key] = false;
-    });
+    // INIT
+    function init() {
+        scene = new THREE.Scene();
+        var directionalLight = new THREE.DirectionalLight({color: 0xFFFFFFF, intensity: 100});
+        directionalLight.position.set(0, 1, 0);
+        directionalLight.castShadow = true;
+        scene.add(directionalLight);
+        var ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+        scene.add(ambientLight);
 
-    function process_keyboard() {
-        let SPEED = 0.15;
-        if (keyboard['p']) {
-            controls.lock();
+        // let grid = new THREE.GridHelper(100, 20, 0x0a0a0a, 0x0a0a0a);
+        // grid.position.set(0, -0.5, 0);
+        // scene.add(grid);
+
+        controls = new THREE.PointerLockControls(camera, document.body);
+        scene.updateMatrixWorld(true);
+
+        function load_model_book() {
+            var gltfLoader = new THREE.GLTFLoader();
+            gltfLoader.load("./data/models/medieval_fantasy_book/scene.gltf", (gltf) => {
+                book = gltf.scene;
+                book.scale.setScalar(20);
+                book.rotation.y = MY_LIBS.degToRad(0);
+                scene.add(book);
+                mixerBook = new THREE.AnimationMixer(book);
+                gltf.animations.forEach((clip) => {
+                    mixerBook.clipAction(clip).play();
+                })
+            });
+        };
+
+        load_model_book();
+
+        const onKeyDown = function ( event ) {
+
+            switch ( event.keyCode ) {
+                
+                case 80: // p
+                    controls.lock();
+                    break;
+                case 38: // up
+                case 87: // w
+                    moveForward = true;
+                    break;
+
+                case 37: // left
+                case 65: // a
+                    moveLeft = true;
+                    break;
+
+                case 40: // down
+                case 83: // s
+                    moveBackward = true;
+                    break;
+
+                case 39: // right
+                case 68: // d
+                    moveRight = true;
+                    break;
+
+                case 32: // space
+                    if ( canJump === true ) velocity.y += 350;
+                    canJump = false;
+                    break;
+
+            }
+
+        };
+
+        const onKeyUp = function ( event ) {
+
+            switch ( event.keyCode ) {
+
+                case 38: // up
+                case 87: // w
+                    moveForward = false;
+                    break;
+
+                case 37: // left
+                case 65: // a
+                    moveLeft = false;
+                    break;
+
+                case 40: // down
+                case 83: // s
+                    moveBackward = false;
+                    break;
+
+                case 39: // right
+                case 68: // d
+                    moveRight = false;
+                    break;
+
+            }
+
+        };
+
+        document.addEventListener( 'keydown', onKeyDown, false );
+        document.addEventListener( 'keyup', onKeyUp, false );
+
+        raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+
+        // objects
+
+        const boxGeometry = new THREE.BoxBufferGeometry( 20, 20, 20 ).toNonIndexed();
+
+        position = boxGeometry.attributes.position;
+        const colorsBox = [];
+
+        for ( let i = 0, l = position.count; i < l; i ++ ) {
+
+            color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+            colorsBox.push( color.r, color.g, color.b );
+
         }
-        if (keyboard['w']) {
-            controls.moveForward(SPEED);
-        }
-        if (keyboard['s']) {
-            controls.moveForward(-SPEED);
-        } 
-        if(keyboard['d']) {
-            controls.moveRight(SPEED);
-        }
-        if(keyboard['a']) {
-            controls.moveRight(-SPEED);
-        }
-        if(keyboard[' ']) {
-            controls.getObject().position.y += 0.01;
-        }
-    };
 
-    var load_model_book = function() {
-        var gltfLoader = new THREE.GLTFLoader();
-        gltfLoader.load("./data/models/medieval_fantasy_book/scene.gltf", (gltf) => {
-            book = gltf.scene;
-            book.scale.setScalar(1);
-            book.rotation.y = MY_LIBS.degToRad(0);
-            book.position.x = 2;
-            // book.rotation.y = MY_LIBS.degToRad(0);
-            scene.add(book);
-            mixerBook = new THREE.AnimationMixer(book);
-            gltf.animations.forEach((clip) => {
-                mixerBook.clipAction(clip).play();
-            })
-        });
-    };
+        boxGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colorsBox, 3 ) );
 
-    var drawScene = function() {
-        requestAnimationFrame(drawScene);
-        renderer.render(scene, cam);
-        process_keyboard();
+        for ( let i = 0; i < 500; i ++ ) {
+            const boxMaterial = new THREE.MeshPhongMaterial( { specular: 0xffffff, flatShading: true, vertexColors: true } );
+            boxMaterial.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+
+            const box = new THREE.Mesh( boxGeometry, boxMaterial );
+            box.position.x = Math.floor( Math.random() * 20 - 10 ) * 100;
+            box.position.y = Math.floor( Math.random() * 20 ) * 20 + 20;
+            box.position.z = Math.floor( Math.random() * 20 - 10 ) * 100;
+
+            scene.add( box );
+            objects.push( box );
+        }
+
+        scene.background = new THREE.TextureLoader().load("./data/background/sky2.jpg");
+        camera.position.set(0, 5, 0);
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize(innerWidth, innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+    }
+    
+    //ANIMATE
+    function animate() {
+        requestAnimationFrame( animate );
         const delta = clock.getDelta();
         mixerBook.update(delta);
-        camPosition.setFromMatrixPosition((controls.getObject()).matrixWorld);
-        console.log("Position: " + camPosition.x + ", " + camPosition.y + ", " + camPosition.z);
-    };
-    load_model_book();
-    drawScene();
+        const time = performance.now();
+
+        if ( controls.isLocked === true ) {
+            raycaster.ray.origin.copy(controls.getObject().position);
+            raycaster.ray.origin.y -= 10;
+            const intersections = raycaster.intersectObjects( objects );
+            const onObject = intersections.length > 0;
+            const delta = ( time - prevTime ) / 1000;
+            velocity.x -= velocity.x * 10.0 * delta;
+            velocity.z -= velocity.z * 10.0 * delta;
+            velocity.y -= 9.8 * 100.0 * delta / 2; // 100.0 = mass
+            direction.z = Number( moveForward ) - Number( moveBackward );
+            direction.x = Number( moveRight ) - Number( moveLeft );
+            direction.normalize(); // this ensures consistent movements in all directions
+            if ( moveForward || moveBackward ) velocity.z -= direction.z * 2000.0 * delta; // cho ni chay nhanh
+            if ( moveLeft || moveRight ) velocity.x -= direction.x * 2000.0 * delta; // cho ni chay nhanh lun
+            if ( onObject === true ) {
+                velocity.y = Math.max( 0, velocity.y );
+                canJump = true;
+            }
+            controls.moveRight( - velocity.x * delta );
+            controls.moveForward( - velocity.z * delta );
+            controls.getObject().position.y += ( velocity.y * delta); // new behavior // cho ni nhay cao
+            if ( controls.getObject().position.y < 10 ) {
+                velocity.y = 0;
+                controls.getObject().position.y = 10;
+                canJump = true;
+            }
+        }
+        prevTime = time;
+        renderer.render( scene, camera );
+    }
 }
