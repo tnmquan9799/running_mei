a92fb57921594cbc9688a954ff313353b5adfa94
Nguyễn Trần Hữu Tuấn <tuannguyencorei7@gmail.com>
2021-01-06 00:15:57 +0700

    Rebase and update

--- js/playground.js
+++ js/playground.js
@@ -1,8 +1,26 @@
 var playground = function () {
-    document.addEventListener("click", function () {
-        document.getElementById("blocker").innerHTML = "";
-        document.getElementById("blocker").style.background = "none";
-        controls.lock();
+    const endgameContainer = document.getElementById("endgame-container");
+    const endgame = document.getElementById("endgame");
+    document.addEventListener("keydown", backToIndex);
+    document.addEventListener("keydown",replay);
+    function replay(event){
+        var x = event.which || event.keyCode;
+        if (x == 13) {
+            location.replace("./gameScreen.html");
+        }
+    };
+    function backToIndex(event) {
+        var x = event.which || event.keyCode;
+        if (x == 8) {
+            location.replace("./index.html");
+        }
+    }
+
+    window.addEventListener("resize", function () {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
     });
 
     const objects = [];
@@ -10,7 +28,7 @@ var playground = function () {
     let camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 1, 1000);
     let controls = new THREE.PointerLockControls(camera, document.body);
     let raycaster;
-    
+
     const listener = new THREE.AudioListener();
     camera.add(listener);
     const sound = new THREE.Audio(listener);
@@ -28,9 +46,6 @@ var playground = function () {
     const velocity = new THREE.Vector3();
     const direction = new THREE.Vector3();
     const color = new THREE.Color();
-
-    const blocker = document.getElementById('blocker');
-    const instructions = document.getElementById('instructions');
 
     init();
     animate();
@@ -67,21 +82,30 @@ var playground = function () {
         };
 
         load_model_book();
-        
-        // INSTRUCTIONS
-        controls.addEventListener('lock', function() {
+
+        const blocker = document.getElementById('blocker');
+        const instructions = document.getElementById('instructions');
+
+        // Endgame popup
+
+
+        instructions.addEventListener('click', function () {
+            controls.lock();
+        }, false);
+
+        controls.addEventListener('lock', function () {
             instructions.style.display = 'none';
             blocker.style.display = 'none';
         });
 
-        controls.addEventListener('unlock', function() {
+        controls.addEventListener('unlock', function () {
             instructions.style.display = '';
             blocker.style.display = 'block';
         });
-        
+
         // SOUND
         const audioLoader = new THREE.AudioLoader();
-        audioLoader.load('./data/audio/forest.ogg', function(buffer) {
+        audioLoader.load('./forest.ogg', function (buffer) {
             sound.setBuffer(buffer);
             sound.setLoop(true);
             sound.setVolume(0.1);
@@ -119,56 +143,66 @@ var playground = function () {
         };
 
         const onKeyUp = function (event) {
+
             switch (event.keyCode) {
+
                 case 38: // up
                 case 87: // w
                     moveForward = false;
                     break;
+
                 case 37: // left
                 case 65: // a
                     moveLeft = false;
                     break;
+
                 case 40: // down
                 case 83: // s
                     moveBackward = false;
                     break;
+
                 case 39: // right
                 case 68: // d
                     moveRight = false;
                     break;
             }
         };
+
         document.addEventListener('keydown', onKeyDown, false);
         document.addEventListener('keyup', onKeyUp, false);
+
         raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, - 1, 0), 0, 10);
 
         // objects
-        const boxGeometry = new THREE.BoxBufferGeometry(20, 20, 20).toNonIndexed();
+
+        const boxGeometry = new THREE.BoxBufferGeometry(20, 10, 30).toNonIndexed();
 
         position = boxGeometry.attributes.position;
         const colorsBox = [];
+
         for (let i = 0, l = position.count; i < l; i++) {
+
             color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
             colorsBox.push(color.r, color.g, color.b);
+
+        }
+        boxGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsBox, 3));
+
+        for (let i = 0; i < 300; i++) {
+            const boxMaterial = new THREE.MeshPhongMaterial({ specular: 0xffffff, flatShading: true, vertexColors: true });
+            boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+
+            const box = new THREE.Mesh(boxGeometry, boxMaterial);
+            box.position.x = Math.floor(Math.random() * 20 - 10) * 100;
+            box.position.y = Math.floor(Math.random() * 20) * 20 + 20;
+            box.position.z = Math.floor(Math.random() * 20 - 10) * 100;
+
+            scene.add(box);
+            objects.push(box);
         }
 
-        boxGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colorsBox, 3 ) );
-
-        for ( let i = 0; i < 300; i ++ ) {
-            const boxMaterial = new THREE.MeshPhongMaterial( { specular: 0xffffff, flatShading: true, vertexColors: true } );
-            boxMaterial.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-
-            const box = new THREE.Mesh( boxGeometry, boxMaterial );
-            box.position.x = Math.floor( Math.random() * 20 - 10 ) * 100;
-            box.position.y = Math.floor( Math.random() * 20 ) * 20 + 20;
-            box.position.z = Math.floor( Math.random() * 20 - 10 ) * 100;
-
-            scene.add( box );
-            objects.push( box );
-
-
         scene.background = new THREE.TextureLoader().load("./data/background/sky2.jpg");
-        camera.position.set(0, 700, 0);
+        camera.position.set(0, 5, 0);
 
         renderer = new THREE.WebGLRenderer();
         renderer.setPixelRatio(window.devicePixelRatio);
@@ -192,7 +226,7 @@ var playground = function () {
             const delta = (time - prevTime) / 1000;
             velocity.x -= velocity.x * 10.0 * delta;
             velocity.z -= velocity.z * 10.0 * delta;
-            velocity.y -= 9.8 * 100.0 * delta / 2; // 100.0 = mass
+            velocity.y -= 9.8 * 100.0 * delta/1.5; // 100.0 = mass
             direction.z = Number(moveForward) - Number(moveBackward);
             direction.x = Number(moveRight) - Number(moveLeft);
             direction.normalize(); // this ensures consistent movements in all directions
@@ -210,18 +244,15 @@ var playground = function () {
                 controls.getObject().position.y = 10;
                 canJump = true;
             }
-            
-            // GAME OVER
 
+            //EndGame
             if (controls.getObject().position.y > 600) {
-                document.getElementById("endgame").style.background = "rgba(0, 0, 0, 0.5)";
-                document.getElementById("endgame").style.display = "block";
-                controls.unlock();
-                console.log("AABABA");
+                endgameContainer.style.display = 'block';
+                endgame.style.display = 'block';
             }
+
         }
         prevTime = time;
         renderer.render(scene, camera);
     }
-}
 }
